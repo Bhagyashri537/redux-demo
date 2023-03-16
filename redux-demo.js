c3fa1d9acdd9b574a64f9507276657811cbefd3b
Bhagyashri <bhagyashrideoghare116@gmail.com>
2023-03-16 15:20:29 +0530

    incrementing and decrementing values in redux

---
+++ redux-demo.js
@@ -0,0 +1,30 @@
+const redux = require('redux')
+
+const counterReducer = (state = {counter : 0} , action) => {
+    if(action.type === 'increment'){
+        return {
+            counter: state.counter+5
+        }
+    }
+    if(action.type === 'decrement'){
+        return {
+            counter: state.counter-1
+        }
+    }
+}
+
+const store = redux.createStore(counterReducer)
+
+const counterSubscriber = () =>{
+    const latestStore = store.getState()
+    console.log(latestStore)
+} 
+
+store.subscribe(counterSubscriber)
+
+store.dispatch({type:'increment'});
+store.dispatch({type:'decrement'});
+store.dispatch({type:'decrement'});
+store.dispatch({type:'decrement'});
+store.dispatch({type:'decrement'});
+store.dispatch({type:'decrement'});
