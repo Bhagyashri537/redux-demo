26c337dcead28b80b9878eb3a2b0f1a2a8ffc82d
Bhagyashri <bhagyashrideoghare116@gmail.com>
2023-03-16 15:26:07 +0530

    increment and decrement by 2

--- redux-demo.js
+++ redux-demo.js
@@ -1,14 +1,14 @@
 const redux = require('redux')
 
 const counterReducer = (state = {counter : 0} , action) => {
-    if(action.type === 'increment'){
+    if(action.type === 'incrementBy2'){
         return {
-            counter: state.counter+5
+            counter: state.counter+2
         }
     }
-    if(action.type === 'decrement'){
+    if(action.type === 'decrementBy2'){
         return {
-            counter: state.counter-1
+            counter: state.counter-2
         }
     }
 }
@@ -24,7 +24,6 @@ store.subscribe(counterSubscriber)
 
 store.dispatch({type:'increment'});
 store.dispatch({type:'decrement'});
-store.dispatch({type:'decrement'});
-store.dispatch({type:'decrement'});
-store.dispatch({type:'decrement'});
-store.dispatch({type:'decrement'});
+
+store.dispatch({type:'incrementBy2'});
+store.dispatch({type:'decrementBy2'});
